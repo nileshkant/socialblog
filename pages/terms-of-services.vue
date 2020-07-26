656b253d81ae12f8c41ec95d97a3355ab72b5e2c
Nilesh Kant <nileshkant0@gmail.com>
2020-07-27 04:03:01 +0530

    Update terms-of-services.vue

--- pages/terms-of-services.vue
+++ pages/terms-of-services.vue
@@ -96,7 +96,75 @@
           own risk.
         </p>
 
-        <h2>7. Site Terms of Use Modifications</h2>
+        <h2>7. Content on the Services</h2>
+        <p>
+          You are responsible for your use of the Services and for any Content
+          you provide, including compliance with applicable laws, rules, and
+          regulations. You should only provide Content that you are comfortable
+          sharing with others. Any use or reliance on any Content or materials
+          posted via the Services or obtained by you through the Services is at
+          your own risk. We do not endorse, support, represent or guarantee the
+          completeness, truthfulness, accuracy, or reliability of any Content or
+          communications posted via the Services or endorse any opinions
+          expressed via the Services. You understand that by using the Services,
+          you may be exposed to Content that might be offensive, harmful,
+          inaccurate or otherwise inappropriate, or in some cases, postings that
+          have been mislabeled or are otherwise deceptive. All Content is the
+          sole responsibility of the person who originated such Content. We may
+          not monitor or control the Content posted via the Services and, we
+          cannot take responsibility for such Content. We reserve the right to
+          remove Content that violates the User Agreement, including for
+          example, copyright or trademark violations or other intellectual
+          property misappropriation, impersonation, unlawful conduct, or
+          harassment.
+        </p>
+        <p>
+          If you believe that your Content has been copied in a way that
+          constitutes copyright infringement, please report this by
+          <NLink to="/contact-us">contacting us</NLink>.
+        </p>
+        <h3>Your Rights and Grant of Rights in the Content</h3>
+        <p>
+          You retain your rights to any Content you submit, post or display on
+          or through the Services. What’s yours is yours — you own your Content
+          (and your incorporated audio, photos and videos are considered part of
+          the Content). By submitting, posting or displaying Content on or
+          through the Services, you grant us a worldwide, non-exclusive,
+          royalty-free license (with the right to sublicense) to use, copy,
+          reproduce, process, adapt, modify, publish, transmit, display and
+          distribute such Content in any and all media or distribution methods
+          now known or later developed (for clarity, these rights include, for
+          example, curating, transforming, and translating). This license
+          authorizes us to make your Content available to the rest of the world
+          and to let others do the same. You agree that this license includes
+          the right for The Open Stories to provide, promote, and improve the
+          Services and to make Content submitted to or through the Services
+          available to other companies, organizations or individuals for the
+          syndication, broadcast, distribution, promotion or publication of such
+          Content on other media and services, subject to our terms and
+          conditions for such Content use. Such additional uses by The Open
+          Stories, or other companies, organizations or individuals, is made
+          with no compensation paid to you with respect to the Content that you
+          submit, post, transmit or otherwise make available through the
+          Services as the use of the Services by you is hereby agreed as being
+          sufficient compensation for the Content and grant of rights herein.
+          The Open Stories has an evolving set of rules for how ecosystem
+          partners can interact with your Content on the Services. These rules
+          exist to enable an open ecosystem with your rights in mind. You
+          understand that we may modify or adapt your Content as it is
+          distributed, syndicated, published, or broadcast by us and our
+          partners and/or make changes to your Content in order to adapt the
+          Content to different media. You represent and warrant that you have,
+          or have obtained, all rights, licenses, consents, permissions, power
+          and/or authority necessary to grant the rights granted herein for any
+          Content that you submit, post or display on or through the Services.
+          You agree that such Content will not contain material subject to
+          copyright or other proprietary rights, unless you have necessary
+          permission or are otherwise legally entitled to post the material and
+          to grant The Open Stories the license described above.
+        </p>
+
+        <h2>8. Site Terms of Use Modifications</h2>
 
         <p>
           The Open Stories may revise these Terms of Use for its Website at any
@@ -118,6 +186,51 @@
         <p>
           Any claim related to The Open Stories's Website shall be governed by
           the laws of in without regards to its conflict of law provisions.
+        </p>
+        <h2>10. Your Account</h2>
+        <p>
+          You may need to create an account to use some of our Services. You are
+          responsible for safeguarding your account, so use a strong password
+          and limit its use to this account. We cannot and will not be liable
+          for any loss or damage arising from your failure to comply with the
+          above. You can control most communications from the Services. We may
+          need to provide you with certain communications, such as service
+          announcements and administrative messages. These communications are
+          considered part of the Services and your account, and you may not be
+          able to opt-out from receiving them.
+        </p>
+        <h2>11. Who May Use the Services</h2>
+        <p>
+          You may use the Services only if you agree to form a binding contract
+          with The Open Stories and are not a person barred from receiving
+          services under the laws of the applicable jurisdiction. In any case,
+          you must be at least 13 years old to use the Services. If you are
+          accepting these Terms and using the Services on behalf of a company,
+          organization, government, or other legal entity, you represent and
+          warrant that you are authorized to do so and have the authority to
+          bind such entity to these Terms, in which case the words “you” and
+          “your” as used in these Terms shall refer to such entity.
+        </p>
+        <h2>
+          12. Your License to Use the Services
+        </h2>
+        <p>
+          The Open Stories gives you a personal, worldwide, royalty-free,
+          non-assignable and non-exclusive license to use the software provided
+          to you as part of the Services. This license has the sole purpose of
+          enabling you to use and enjoy the benefit of the Services as provided
+          by The Open Stories, in the manner permitted by these Terms. The
+          Services are protected by copyright, trademark, and other laws of both
+          the United States and other countries. Nothing in the Terms gives you
+          a right to use the The Open Stories name or any of the The Open
+          Stories trademarks, logos, domain names, other distinctive brand
+          features, and other proprietary rights. All right, title, and interest
+          in and to the Services (excluding Content provided by users) are and
+          will remain the exclusive property of The Open Stories and its
+          licensors. Any feedback, comments, or suggestions you may provide
+          regarding The Open Stories, or the Services is entirely voluntary and
+          we will be free to use such feedback, comments or suggestions as we
+          see fit and without any obligation to you.
         </p>
       </v-container>
     </v-content>
